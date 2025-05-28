77e3cf7f7f26e4cce79a40ebedac8774c9a11567
masxkip <jesufemiketiku@gmail.com>
2025-05-27 20:38:57 -0700

    new update

--- src/data/excavatorsData.js
+++ src/data/excavatorsData.js
@@ -1,4 +1,27 @@
 const excavatorsData = [
+
+      {
+     id: 203,
+      name: "Kubota KX080-4",
+      price: "Call for Prices",
+      images: [
+        "/images/i4/i4a.jpeg", "/images/i4/i4b.jpeg", "/images/i4/i4c.jpeg",
+        "/images/i4/i4d.jpeg", "/images/i4/i4e.jpeg", "/images/i4/i4f.jpeg",
+      ],
+      year: 2024,
+      category: "Compact Excavator",
+      phone: "+1 543-201-5678",
+      location: "Vancouver",
+      companyEmail: "info@catdealers.com",
+      manufacturer:"KUBOTA",
+      model: "KX040-4",
+      condition: "Used",
+      serialNumber: "KBCDZ84CTP3J16345",
+      description: "Base Unit, 66HP Bobcat Turbo Diesel Engine, Enclosed ROPS Cab with Heat & A/C, Suspension Seat, Joystick Controls, Hydraulic Quick Coupler, Auxiliary Hydraulics, 2-Speed Travel, Rubber Tracks, 18,977 lb Operating Weight, 3,650 lb Max Bucket Force, Long Arm Option for Extended Reach, and 11.5 ft Max Dig Depth. Tracks and Undercarriage in Good Condition. Current Machine Hours: 1,825. Machine Operates Well with Excellent Visibility and Smooth Hydraulics, Ideal for Utility, Residential, and General Construction Work."
+    },
+
+
+
   {
       id: 201,
       name: "Bobcat E85 Compact",
@@ -12,11 +35,32 @@ const excavatorsData = [
       phone: "+1 543-201-5678",
       location: "Vancouver",
       companyEmail: "info@catdealers.com",
-      manufacturer:"Caterpillar",
+      manufacturer:"BOBCAT",
       model: "Bobcat E85",
       condition: "Used",
       serialNumber: "T4200475",
       description: "Base Unit, 66HP Bobcat Turbo Diesel Engine, Enclosed ROPS Cab with Heat & A/C, Suspension Seat, Joystick Controls, Hydraulic Quick Coupler, Auxiliary Hydraulics, 2-Speed Travel, Rubber Tracks, 18,977 lb Operating Weight, 3,650 lb Max Bucket Force, Long Arm Option for Extended Reach, and 11.5 ft Max Dig Depth. Tracks and Undercarriage in Good Condition. Current Machine Hours: 1,825. Machine Operates Well with Excellent Visibility and Smooth Hydraulics, Ideal for Utility, Residential, and General Construction Work."
+    },
+
+
+    {
+    id: 204,
+      name: "CAT 306CR Compact Excavator",
+      price: "Call for Prices",
+      images: [
+        "/images/i6/i6a.jpeg", "/images/i6/i6b.jpeg", "/images/i6/i6c.jpeg",
+        "/images/i6/i6d.jpeg", "/images/i6/i6e.jpeg", "/images/i6/i6f.jpeg",
+      ],
+      year:  2023,
+      category: "Compact Excavator",
+      phone: "+1 543-201-5678",
+      location: "Vancouver",
+      companyEmail: "info@catdealers.com",
+      manufacturer:"CATERPILLAR",
+      model: "306CR",
+      condition: "Used",
+      serialNumber: "6G608403",
+      description: "Base Unit, 2023 Model, 867 Verified Hours, 2D Ease of Use Installed, 36” Bucket, Hydraulic Thumb, Hydraulic Quick Coupler, Enclosed ROPS Cab with Bluetooth Radio, Heat & A/C, Extra Counterweight, and Standard Joystick Controls. This 6-ton class machine includes 2-speed travel and auxiliary hydraulics. Operating condition is solid with regular wear; minor dent on front panel and one boom light is broken/missing. Cab mirror is also missing. Machine is protected by Caterpillar's current 48-month / 2500-hour warranty valid through March 29, 2027."
     },
 
     {
@@ -32,13 +76,14 @@ const excavatorsData = [
       phone: "+1 543-201-5678",
       location: "Vancouver",
       companyEmail: "info@catdealers.com",
-      manufacturer:"Caterpillar",
+      manufacturer:"CATERPILLAR",
       model: " 308E2 CR",
       condition: "Used",
       serialNumber: "CAT0308EJFJX1265",
       description: "Base Unit, 66HP Bobcat Turbo Diesel Engine, Enclosed ROPS Cab with Heat & A/C, Suspension Seat, Joystick Controls, Hydraulic Quick Coupler, Auxiliary Hydraulics, 2-Speed Travel, Rubber Tracks, 18,977 lb Operating Weight, 3,650 lb Max Bucket Force, Long Arm Option for Extended Reach, and 11.5 ft Max Dig Depth. Tracks and Undercarriage in Good Condition. Current Machine Hours: 1,825. Machine Operates Well with Excellent Visibility and Smooth Hydraulics, Ideal for Utility, Residential, and General Construction Work."
     },
    
+    
     
 ];
 
--- src/data/loadersData.js
+++ src/data/loadersData.js
@@ -1,18 +1,23 @@
 const loadersData = [
   {
-    id: 201,
-    name: "John Deere 524L Loader",
-    price: "$45,000",
-    images: [
-      "/images/loader1-1.jpg", "/images/loader1-2.jpg", "/images/loader1-3.jpg",
-      "/images/loader1-4.jpg", "/images/loader1-5.jpg", "/images/loader1-6.jpg",
-      "/images/loader1-7.jpg"
-    ],
-    phone: "+1 543-202-5678",
-    email: "sales@johndeeredealers.com",
-    companyName: "John Deere Equipment",
-    companyEmail: "info@johndeeredealers.com"
-  },
+  id: 301,
+      name: "CAT 262D3 Skid Steer Loader",
+      price: "Call for Prices",
+      images: [
+        "/images/i5/i5a.jpeg", "/images/i5/i5b.jpeg", "/images/i5/i5c.jpeg",
+        "/images/i5/i5d.jpeg", "/images/i5/i5e.jpeg", "/images/i5/i5f.jpeg",
+      ],
+      year: 2022,
+      category: "Loader",
+      phone: "+1 543-201-5678",
+      location: "Vancouver",
+      companyEmail: "info@catdealers.com",
+      manufacturer:"KUBOTA",
+      model: " 262D56",
+      condition: "Used",
+      serialNumber: "ZB203216",
+      description: "Base Unit, 74” Bucket Included, 79 Verified Hours, High-Flow Hydraulics, Hydraulic Quick Attach, Enclosed ROPS Cab with Standard Operator Controls, Two-Speed Travel with Max Speed of 11 mph, 2022 Model Year, Sold “As-Is Where-Is.” The unit starts, runs, and drives. Full operational testing not completed. "
+    },
 ];
 
 export default loadersData;
