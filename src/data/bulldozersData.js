c9d3430191321950c9945e9ad11d79c6fccd75fd
masxkip <jesufemiketiku@gmail.com>
2025-05-27 19:58:04 -0700

    new update

--- src/data/bulldozersData.js
+++ src/data/bulldozersData.js
@@ -4,8 +4,8 @@ const bulldozersData = [
       name: "CAT D5C XL Hystat",
       price: "Call for Prices",
       images: [
-        "/images/i1/i1a.jpeg", "/images/i1/i1b.jpeg", "/images/i1/i1c.jpeg",
-        "/images/i1/i1d.jpeg", "/images/i1/i1e.jpeg", "/images/i1/i1f.jpeg",
+        "/images/i1/i1a.jpeg", "/images/i1/i1t.jpeg", "/images/i1/i1g.jpeg",
+        "/images/i1/i1k.jpeg", "/images/i1/i1m.jpeg", "/images/i1/i1h.jpeg",
       ],
       year: 2022,
       phone: "+1 543-201-5678",
@@ -14,7 +14,7 @@ const bulldozersData = [
       manufacturer:"CATERPILLAR",
       category: "Bulldozer",
       model: "D5C XL HST III",
-      condition: "New",
+      condition: "Used",
       serialNumber: "7PS00422",
       description: "Base Unit, 90HP Caterpillar Turbo Diesel Engine, Open ROPS Canopy, Cloth Suspension Seat, Joystick Controls, Hydrostatic Transmission, 2-Speed Travel, 6-Way Tilt Blade, 19,316 lb Operating Weight, 6.2 mph Max Travel Speed, Steel Tracks with 70% Remaining, Pads at 65%, Sprockets at 40%. Features include Grade Control, Undercarriage Guards, and Protective Screens. Undercarriage in Good Operating Condition (Sprockets Showing Wear). This Machine Runs and Operates Well. Please Call or Email to Schedule a Viewing."
     },
--- src/data/excavatorsData.js
+++ src/data/excavatorsData.js
@@ -1,21 +1,42 @@
 const excavatorsData = [
   {
-      id: 101,
-      name: "CAT 320 Excavator",
+      id: 201,
+      name: "Bobcat E85 Compact",
       price: "Call for Prices",
       images: [
-        "/images/e2.jpg", "/images/h4.jpg", "/images/e2.jpg",
-        "/images/e2.jpg", "/images/h4.jpg", "/images/hn1.jpg",
-        "/images/e1.png"
+        "/images/i2/i2f.jpeg", "/images/i2/i2h.jpeg", "/images/i2/i2k.jpeg",
+        "/images/i2/i2i.jpeg", "/images/i2/i2d.jpeg", "/images/i2/i2a.jpeg",
       ],
+      year: 2018,
+      category: "Compact Excavator",
       phone: "+1 543-201-5678",
       location: "Vancouver",
       companyEmail: "info@catdealers.com",
       manufacturer:"Caterpillar",
-      model: "XC2350",
-      condition: "New",
-      serialNumber: "200475",
-      description: "XC2350 getting a nwe air conditoner from temu, can you guess in what we want in ram, XC2350 getting a nwe air conditoner from temu, can you guess in what we want in ram,vXC2350 getting a nwe air conditoner from temu, can you guess in what we want in ram,XC2350 getting a nwe air conditoner from temu, can you guess in what we want in ramXC2350 getting a nwe air conditoner from temu, can you guess in what we want in ramXC2350 getting a nwe air conditoner from temu, can you guess in what we want in ram"
+      model: "Bobcat E85",
+      condition: "Used",
+      serialNumber: "T4200475",
+      description: "Base Unit, 66HP Bobcat Turbo Diesel Engine, Enclosed ROPS Cab with Heat & A/C, Suspension Seat, Joystick Controls, Hydraulic Quick Coupler, Auxiliary Hydraulics, 2-Speed Travel, Rubber Tracks, 18,977 lb Operating Weight, 3,650 lb Max Bucket Force, Long Arm Option for Extended Reach, and 11.5 ft Max Dig Depth. Tracks and Undercarriage in Good Condition. Current Machine Hours: 1,825. Machine Operates Well with Excellent Visibility and Smooth Hydraulics, Ideal for Utility, Residential, and General Construction Work."
+    },
+
+    {
+     id: 202,
+      name: "CAT 308E2 CR Compact Excavator",
+      price: "Call for Prices",
+      images: [
+        "/images/i3/i3d.jpeg", "/images/i3/i3c.jpeg", "/images/i3/i3g.jpeg",
+        "/images/i3/i3f.jpeg", "/images/i3/i3n.jpeg", "/images/i3/i3m.jpeg",
+      ],
+      year: 2018,
+      category: "Compact Excavator",
+      phone: "+1 543-201-5678",
+      location: "Vancouver",
+      companyEmail: "info@catdealers.com",
+      manufacturer:"Caterpillar",
+      model: " 308E2 CR",
+      condition: "Used",
+      serialNumber: "CAT0308EJFJX1265",
+      description: "Base Unit, 66HP Bobcat Turbo Diesel Engine, Enclosed ROPS Cab with Heat & A/C, Suspension Seat, Joystick Controls, Hydraulic Quick Coupler, Auxiliary Hydraulics, 2-Speed Travel, Rubber Tracks, 18,977 lb Operating Weight, 3,650 lb Max Bucket Force, Long Arm Option for Extended Reach, and 11.5 ft Max Dig Depth. Tracks and Undercarriage in Good Condition. Current Machine Hours: 1,825. Machine Operates Well with Excellent Visibility and Smooth Hydraulics, Ideal for Utility, Residential, and General Construction Work."
     },
    
     
