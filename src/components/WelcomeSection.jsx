1754ae18f3c8835360f277234a4190b6d3e2b64c
masxkip <jesufemiketiku@gmail.com>
2025-05-30 06:56:24 -0700

    new update

--- src/components/WelcomeSection.jsx
+++ src/components/WelcomeSection.jsx
@@ -6,9 +6,9 @@ const WelcomeSection = () => {
       <hr />
 
       <div className="welcome-visuals">
-        <img src="/images/Hus1.jpg" alt="Sparks, NV Location" />
+        <img src="/images/Hus1_2.jpg" alt="Sparks, NV Location" />
         <img src="/images/aab.png" alt="Company Logo" className="logo-center" />
-        <img src="/images/Hus1.jpg" alt="Nampa, ID Location" />
+        <img src="/images/Hus1_2.jpg" alt="Nampa, ID Location" />
       </div>
 
       <p className="welcome-description">
--- src/data/bulldozersData.js
+++ src/data/bulldozersData.js
@@ -18,6 +18,28 @@ const bulldozersData = [
       serialNumber: "7PS00422",
       description: "Base Unit, 90HP Caterpillar Turbo Diesel Engine, Open ROPS Canopy, Cloth Suspension Seat, Joystick Controls, Hydrostatic Transmission, 2-Speed Travel, 6-Way Tilt Blade, 19,316 lb Operating Weight, 6.2 mph Max Travel Speed, Steel Tracks with 70% Remaining, Pads at 65%, Sprockets at 40%. Features include Grade Control, Undercarriage Guards, and Protective Screens. Undercarriage in Good Operating Condition (Sprockets Showing Wear). This Machine Runs and Operates Well. Please Call or Email to Schedule a Viewing."
     },
+
+     {
+      id: 102,
+      name: "CAT D4K2 XL Bulldozer",
+      price: "Call for Price",
+      images: [
+        "/images/i7/i71.jpeg", "/images/i7/i72.jpeg", "/images/i7/i73.jpeg",
+        "/images/i7/i74.jpeg", "/images/i7/i75.jpeg", "/images/i7/i76.jpeg",
+      ],
+      year: 2016,
+      phone: "+1 543-201-5678",
+      location: "Vancouver",
+      companyEmail: "info@catdealers.com",
+      manufacturer:"CATERPILLAR",
+      category: "Bulldozer",
+      model: " D4K2 XL",
+      condition: "Used",
+      serialNumber: "CAT00D4KJKM202174",
+      description: "Base Unit, 92HP Caterpillar Turbo Diesel Engine, Enclosed ROPS Cab with Heat & A/C, Cloth Suspension Seat, Joystick Controls, Hydrostatic Transmission, 6-Way Blade Tilt, Factory Winch, 18,080 lb Operating Weight, 6.2 mph Max Travel Speed. Good Undercarriage with 70% Remaining (Tracks, Pads, and Rollers at 70%, Sprockets at 75%). Features include Grade Control, Undercarriage Guards, and Protective Screens. Machine Runs and Operates Very Well with Only 994 Hours. Winch Functions Properly. Please Call or Email to Schedule a Viewing."
+    },
+
+   
  
 ];
 
